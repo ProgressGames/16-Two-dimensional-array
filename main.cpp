9790c5f54a1842d4b848e6aa66db4e9466777d9c
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-09-29 19:08:57 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+// Двумерные массивы
+
+int main() {
+    const int ROW = 2;
+    const int COL = 4;
+
+    int arr[ROW][COL]
+    {
+        {10,20,30,40},
+        {100,200,300,400}
+    };
+
+    cout << arr[1][2] << endl;
+
+    int a=0;
+}
